4dbc6fe1a08c713599c31bed21585fb0a6139751
Darby Mecham <dloumech@byu.edu>
2023-02-13 22:49:04 -0700

    Add project files.

---
+++ Mission6/Models/NewMovie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission6AssignmentDarbyMecham.Models;
+
+namespace Mission6AssignmentDarbyMecham.Models
+{
+    public class NewMovie
+    {
+        //category, title, year, director, edited, lent to, notes
+        [Required]
+        public string MovieCategory { get; set; }
+
+        [Required] 
+        public string MovieTitle { get; set; }
+        [Required]
+        public string MovieYear { get; set; }
+        [Required]
+        public string MovieDirector { get; set; }
+        [Required]
+        public string MovieRating { get; set; }
+        
+        //optional fields 
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+
+        [MaxLength(25)]
+        public string Notes { get; set; }
+    }
+}
---
+++ Mission6/Models/NewMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6AssignmentDarbyMecham.Models
+{
+    public class NewMovieContext : DbContext
+    {
+        //constructor
+        public NewMovieContext (DbContextOptions<NewMovieContext> options) : base (options)
+        {
+            //leave blank for now
+        }
+
+        public DbSet<NewMovie> newMovies { get; set; }
+    }
+}
